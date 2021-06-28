747da8ddb96f5694f4d7422af6838bdab536f897
Milad Behzadi <milad.bhzdi@gmail.com>
2021-06-28 20:37:43 +0200

    adding logo and logout icon to header

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { ThemeProvider } from 'styled-components';
 import Theme from './components/assets/styles/Theme';
 import GlobalStyle from './components/assets/styles/GlobalStyle';
 import UserProfile from './components/UserProfile/UserProfile';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <ThemeProvider theme={Theme} />
+      <Header />
       <GlobalStyle />
       <UserProfile />
     </div>
