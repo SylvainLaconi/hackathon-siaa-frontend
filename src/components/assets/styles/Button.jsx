75e49f40ff05ba6a3da06c93ff85058c21125f07
Palabbe <pauline.labbe7@gmail.com>
2021-06-28 15:18:25 +0200

    création de la structure + premier jet du theme avec styled-component

---
+++ src/components/assets/styles/Button.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import Theme from './Theme';
+
+const Button = styled.button`
+  background-color: transparent;
+  color: ${Theme.fiverrGreen};
+  border: ${Theme.fiverrGreen} solid 2px;
+  border-radius: 0.5rem;
+  padding: 0.7rem;
+  border: none;
+  width: 10rem;
+  margin-top: 1rem;
+  font-family: 'Arial', sans-serif;
+  &:hover {
+    background-color: ${Theme.fiverrGreen};
+    color: white;
+  }
+`;
+
+export default Button;
---
+++ src/components/assets/styles/Theme.jsx
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+
+export const Sizes = {
+  mobile: '425px',
+  tablet: '768px',
+  laptop: '1024px',
+  desktop: '2560px',
+};
+
+export const Devices = {
+  mobile: `{min-width: ${Sizes.mobile}}`,
+  tablet: `{min-width: ${Sizes.tablet}}`,
+  laptop: `{min-width: ${Sizes.laptop}}`,
+  desktop: `{min-width: ${Sizes.desktop}}`,
+};
+
+export const Container = styled.div`
+  display: ${(props) => (props.flex ? 'flex' : 'block')};
+  flex-direction: ${(props) => (props.column ? 'column' : 'row')};
+  ${(props) => props.aiCenter && 'align-items:center'};
+  ${(props) => props.acCenter && 'align-content:center'};
+  ${(props) => props.jcCenter && 'justify-content:center'};
+`;
+
+export const Title = styled.h1`
+  font-family: 'Arial', sans-serif;
+  font-size: 1.5rem;
+`;
+
+const Theme = {
+  fiverrGreen: '#1dbf73',
+  fiverrYellow: '#d0e500',
+  fiverrPink: '#ff80ae',
+  fiverrOrange: '#ff7640',
+  fiverrGreenLight: '#d0f7e6',
+  fiverrYellowLight: '#f1f4cb',
+  fiverrPinkLight: '#ffd3e2',
+  fiverrOrangeLight: '#ffe0d4',
+  fiverrGreenMedium: '#00732e',
+  fiverrYellowMedium: '#687200',
+  fiverrPinkMedium: '#be5272',
+  fiverrOrangeMedium: '#8f2900',
+  fiverrGreenDark: '#003912',
+  fiverrYellowDark: '#254200',
+  fiverrPinkDark: '#4d1727',
+  fiverrOrangeDark: '#421300',
+};
+
+export default Theme;
