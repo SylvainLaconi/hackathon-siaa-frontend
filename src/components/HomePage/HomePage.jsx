5dcabfc9bc5892cc89cf18ac5b83c91a89e026b5
Palabbe <pauline.labbe7@gmail.com>
2021-06-30 00:01:10 +0200

    ajout image home

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Title, Container } from '../assets/styles/Theme';
+import styled from 'styled-components';
+import Theme, { Title, Container } from '../assets/styles/Theme';
+import welcome from '../assets/img/welcome.jpg';
+
+const ComponentContainer = styled(Container)`
+  margin-top: 3%;
+`;
+const WelcomePicture = styled.img`
+  width: 60%;
+`;
+const Text = styled.p`
+  color: ${Theme.fiverrOrange};
+  font-family: 'Arial', sans-serif;
+  font-weight: 500;
+`;
 
 const HomePage = () => {
   return (
-    <Container flex column aiCenter acCenter jcCenter>
-      <Title>Welcome on Fiverr Community</Title>
-      <Title>Please log you in ...</Title>
-    </Container>
+    <ComponentContainer flex column aiCenter acCenter jcCenter>
+      <Title>Welcome on Fiverr Community !</Title>
+      <Text>Please login to discover our strong and active community</Text>
+      <WelcomePicture src={welcome} />
+    </ComponentContainer>
   );
 };
 
