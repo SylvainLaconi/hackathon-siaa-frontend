b0a7e0af2fdbeed5b686657f8e0f7321bd238b3a
Sylvain Laconi <sylvain.laconi@gmail.com>
2021-06-30 15:58:25 +0200

    axios post new content

--- src/components/NewPost/NewPost.jsx
+++ src/components/NewPost/NewPost.jsx
@@ -14,6 +14,7 @@ import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import UserContext from '../assets/UserContext';
 
 const useStyles = makeStyles((theme) => ({
@@ -34,17 +35,21 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function NewPost() {
-  const { userInfo, loadingInfo } = useContext(UserContext);
+  const { userInfo, loadingInfo, newChange, setNewChange } =
+    useContext(UserContext);
   const [contentType, setContentType] = React.useState('event');
   const [community, setCommunity] = React.useState('');
   const [communityList, setCommunityList] = useState([]);
   const [loadingList, setLoadingList] = useState(true);
   const [postCategory, setPostCategory] = useState([]);
   const [loadingCategory, setLoadingCategory] = useState(true);
+  const [title, setTitle] = useState('');
+  const [picture, setPicture] = useState('');
+  const [content, setContent] = useState('');
+  const [date, setDate] = useState('');
+  const [location, setLocation] = useState('');
 
   const userId = !loadingInfo && userInfo.user_id;
-
-  console.log(userId);
 
   const getCommunityList = async () => {
     try {
@@ -83,6 +88,27 @@ export default function NewPost() {
   const classes = useStyles();
   const handleContentType = (event, newContentType) => {
     setContentType(newContentType);
+  };
+
+  const postNewPost = async () => {
+    try {
+      await axios.post('http://localhost:8000/api/post', {
+        title,
+        picture,
+        content,
+        date,
+        location,
+        community_id: community,
+        user_id: userId,
+        post_category_id: contentType,
+      });
+      setNewChange(!newChange);
+      toast.success('Your post is sent');
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log(error);
+      toast.error(`${error.message}`);
+    }
   };
   return (
     !loadingList &&
@@ -143,15 +169,41 @@ export default function NewPost() {
           </ToggleButtonGroup>
         </Box>
         <Box display="flex" m="20px" gridGap="20px">
-          <TextField id="standard-basic" label="Title" />
-          <TextField id="standard-basic" label="Picture" />
-          <TextField id="standard-basic" label="Description" />
-          <TextField id="standard-basic" label="Date" />
-          <TextField id="standard-basic" label="Lieu" />
+          <TextField
+            id="standard-basic"
+            label="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <TextField
+            id="standard-basic"
+            label="Picture"
+            value={picture}
+            onChange={(e) => setPicture(e.target.value)}
+          />
+          <TextField
+            id="standard-basic"
+            label="Description"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+          <TextField
+            id="standard-basic"
+            label="Date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+          <TextField
+            id="standard-basic"
+            label="Lieu"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
           <Button
             variant="contained"
             color="primary"
             className={classes.button}
+            onClick={postNewPost}
           >
             Post
           </Button>
